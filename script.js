1ab7b7693e79db639a663c34d886faa5e49ebd53
RahulBosamia <45514766+RahulBosamia@users.noreply.github.com>
2023-07-05 03:26:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function fetchJoke() {
 
 // Translate a text using Google Translate API
 async function translateText(text, targetLanguage) {
-    const apiKey = 'AIzaSyA2mXrYaOsFpziINhj8qy1W2FFuRLtEG7A';
+    const apiKey = '';
     const url = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
   
     try {
