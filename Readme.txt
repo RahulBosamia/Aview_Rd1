e5753c7e000c99c6caee9c784973ad303bb30841
RahulBosamia <rnb200003@utdallas.edu>
2023-07-05 03:18:52 -0500

    first commit

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+1) To run the application unzip the folder and click on the index.html icon. Or open the unziped folder in vscode and in index.html file click on "Go Live" option.
+
+2) Approach to the challenge:
+   a) In the main page of the application I made two sections, in the upper section a new joke is fetched and displayed each time the web page is refreshed
+      and in the lower section the translated joke in the chosen language is displayed.
+   b) The user can choose the language from the drop down to translate the displayed joke.
+   c) I have different functions to fetch, translate and display the joke. In each function potential error handling is done.
+   d) I have used "jokeapi" to fetch the joke and "Google Translate" api for the joke translation.
+   e) Also I have applied necessary styling to make the web page user friendly.
---
+++ script.js
@@ -0,0 +1,89 @@
+// Fetch a random joke from JokeAPI
+async function fetchJoke() {
+    try {
+      const response = await fetch('https://v2.jokeapi.dev/joke/Any');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log('Error fetching joke:', error);
+    }
+  }
+  
+
+// Translate a text using Google Translate API
+async function translateText(text, targetLanguage) {
+    const apiKey = 'AIzaSyA2mXrYaOsFpziINhj8qy1W2FFuRLtEG7A';
+    const url = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
+  
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          q: text,
+          target: targetLanguage,
+        }),
+      });
+      const data = await response.json();
+      return data.data.translations[0].translatedText;
+    } catch (error) {
+      console.log('Error translating text:', error);
+    }
+  }
+  
+  
+  
+
+  // Display the fetched joke on the page
+function displayJoke(joke) {
+    const jokeElement = document.getElementById('joke');
+    jokeElement.innerHTML = '';
+  
+    if (joke.type === 'single') {
+      jokeElement.innerHTML = `<p>${joke.joke}</p>`;
+    } else if (joke.type === 'twopart') {
+      jokeElement.innerHTML = `<p>${joke.setup}</p><p>${joke.delivery}</p>`;
+    } else {
+      jokeElement.innerHTML = '<p>Failed to fetch joke.</p>';
+    }
+  }
+  
+  
+  
+  // Translate the fetched joke to the selected language
+async function translateJoke() {
+    const languageSelect = document.getElementById('language');
+    const selectedLanguage = languageSelect.value;
+    const jokeElement = document.getElementById('joke');
+    const translatedJokeElement = document.getElementById('translatedJoke');
+    translatedJokeElement.innerHTML = '';
+  
+    if (jokeElement.textContent === '') {
+      // If no joke is fetched yet, display an error message
+      translatedJokeElement.innerHTML = '<p>Please fetch a joke first.</p>';
+      return;
+    }
+  
+    const jokeText = jokeElement.textContent;
+    const translatedJoke = await translateText(jokeText, selectedLanguage);
+  
+    if (translatedJoke === undefined) {
+      // If translation fails, display an error message
+      translatedJokeElement.innerHTML = '<p>Translation failed. Please try again.</p>';
+    } else {
+      translatedJokeElement.innerHTML = `<p>${translatedJoke}</p>`;
+    }
+  }
+  
+  
+  // Event listener for the "Translate" button
+  document.getElementById('translateBtn').addEventListener('click', translateJoke);
+  
+  // Fetch a joke and display it on page load
+  window.addEventListener('DOMContentLoaded', async () => {
+    const joke = await fetchJoke();
+    displayJoke(joke);
+  });
+  
